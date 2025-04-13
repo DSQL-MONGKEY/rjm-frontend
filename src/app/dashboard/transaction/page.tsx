2893ab7f82290680e8cd36b73f011bade8671431
Dimpfe <dimasprasetyo7696@gmail.com>
2025-04-13 20:50:49 +0700

    feat<>: initial create

--- src/app/dashboard/transaction/page.tsx
+++ src/app/dashboard/transaction/page.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link';
 import { SearchParams } from 'nuqs/server';
 import { Suspense } from 'react';
 import ProductListingPage from '@/features/products/components/product-listing';
-import ProductTableAction from '@/features/products/components/product-tables/product-table-action';
+import ProductTableAction from '@/features/products/components/product-tables/order-table-action';
 
 export const metadata = {
   title: 'Dashboard: Orders'

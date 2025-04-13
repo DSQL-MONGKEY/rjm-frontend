4e7e6102d80b7f15aa1e0443d70380e85a96056d
Dimpfe <dimasprasetyo7696@gmail.com>
2025-04-13 20:54:51 +0700

    feat<>: create order, customer, employee, orderDetails types

--- types/index.ts
+++ types/index.ts
@@ -33,3 +33,63 @@ export interface FooterItem {
 export type MainNavItem = NavItemWithOptionalChildren;
 
 export type SidebarNavItem = NavItemWithChildren;
+
+export interface Customer {
+  id: number
+  name: string
+  address: string
+  phone: string
+  email: string
+  created_at: string
+  updated_at: string
+}
+
+export interface Employee {
+  id: number
+  base_salary: string
+  fullname: string
+  roles_id: number
+  pivot?: {
+    employee_id: number
+    order_id: number
+  }
+  updated_at: string
+  created_at: string
+}
+
+export interface OrderDetails  {
+  id: number,
+  order_id: number,
+  packet_id: number,
+  packet_name?: string,
+  price: string,
+  subtotal: string,
+  quantity: number,
+  is_custom: boolean,
+  packet?: {
+    id: number,
+    name: string
+  },
+  created_at?: string,
+  updated_at?: string,
+}
+
+export interface Order  {
+  id: number,
+  customer_id?: number,
+  status?: string,
+  token_tracking?: string,
+  invoice_number?: string
+  plat_number?: string,
+  total_price?: string,
+  note?: string,
+  car_merk?: string | null,
+  created_at: string,
+  updated_at: string,
+  customer?: Customer[]
+  order_details?: OrderDetails[]
+  employees?: Employee[]
+}
+
+
+
